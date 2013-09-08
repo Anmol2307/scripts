4ce9e4c9249cae005910102aef53c976dbf4aa30
Philip J Fry <prithviraj.m.billa@gmail.com>
2013-09-08 21:48:53 +0530

    now you can comment

--- birthday.py
+++ birthday.py
@@ -1,6 +1,8 @@
 ## commenting
 import json
 
+import urllib
+
 import urllib2
 
 userid = "userid"
@@ -27,4 +29,27 @@ req = urllib2.Request(posts_url)
 response = urllib2.urlopen(req)
 the_page = response.read()
 
-print the_page
+array = json.loads(the_page);
+
+array = array["data"];
+
+for item in array:
+
+	message = "None Type"
+	if item.has_key("message"):
+		message = item["message"]
+
+	object_id = "None Type"
+	if item.has_key("object_id"):
+		object_id = item["object_id"]
+		print "message:-- ",message
+		print object_id
+
+		print "Type your comment!!"
+		comment_message = raw_input()
+		data = urllib.urlencode({'message': comment_message,'access_token':access_token})
+		comment_url = "https://graph.facebook.com/"+object_id+"/comments"
+		u = urllib2.urlopen(comment_url, data)
+
+		for line in u.readlines():
+  			print line
